ee0a1b9d40f69f6c4365912a4f7e013e03b700e2
Vinicius Vicente <viniciuskmv@gmail.com>
2020-09-08 16:48:39 -0300

    feat: add login view

---
+++ src/presentation/pages/login/login.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Styles from './login-styles.scss'
+import Spinner from '../../components/spinner/spinner'
+import { logo } from '../../components/logo/logo'
+
+const Login: React.FC = () => {
+  return (
+    <div className={Styles.login}>
+      <header className={Styles.header}>
+        <img src={logo} alt="Logo"/>
+        <h1>Enquetes para todos</h1>
+      </header>
+      <form className={Styles.form}>
+        <h2>Login</h2>
+        <div className={Styles.inputWrap}>
+          <input type="email" name="email" placeholder="Digite seu e-mail" />
+          <span className={Styles.status}>🔴</span>
+        </div>
+        <div className={Styles.inputWrap}>
+          <input type="password" name="password" placeholder="Digite sua senha" />
+          <span className={Styles.status}>🔴</span>
+        </div>
+
+        <button className={Styles.submit} type="submit">Login</button>
+        <span className={Styles.link}>Criar conta</span>
+        <div className={Styles.errorWrap}>
+          <Spinner className={Styles.spinner} />
+          <span className={Styles.error}>Erro</span>
+        </div>
+      </form>
+      <footer className={Styles.footer}></footer>
+    </div>
+  )
+}
+
+export default Login
