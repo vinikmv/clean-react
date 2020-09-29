32c2b2885d19754500e3fe5385b32d876ce6886b
Vinicius Vicente <viniciuskmv@gmail.com>
2020-09-29 14:49:24 -0300

    refactor: change Validation to receive an object instead of value

--- src/validators/builder/validation-builder.spec.ts
+++ src/validators/builder/validation-builder.spec.ts
@@ -1,5 +1,5 @@
 import { ValidationBuilder as sut } from './validation-builder'
-import { RequiredFieldValidation, EmailValidation, MinLengthValidation } from '@/validators'
+import { RequiredFieldValidation, EmailValidation, MinLengthValidation, CompareFieldsValidation } from '@/validators'
 import faker from 'faker'
 
 describe('ValidationBuilder', () => {
@@ -15,11 +15,18 @@ describe('ValidationBuilder', () => {
     expect(validations).toEqual([new EmailValidation(field)])
   })
 
-  test('Should return EmailValidation', () => {
+  test('Should return MinLengthValidation', () => {
     const field = faker.database.column()
     const length = faker.random.number()
     const validations = sut.field(field).min(length).build()
     expect(validations).toEqual([new MinLengthValidation(field, length)])
+  })
+
+  test('Should return CompareFieldsValidation', () => {
+    const field = faker.database.column()
+    const fieldToCompare = faker.database.column()
+    const validations = sut.field(field).sameAs(fieldToCompare).build()
+    expect(validations).toEqual([new CompareFieldsValidation(field, fieldToCompare)])
   })
 
   test('Should return a list of validations', () => {
